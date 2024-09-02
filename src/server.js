cc78cebf7d2d3870076a70abc74cef03e7e09bfd
novi <novi_dewi@kamoro.com>
2024-09-02 23:27:11 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 8081,
-    // eslint-disable-next-line no-undef
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
