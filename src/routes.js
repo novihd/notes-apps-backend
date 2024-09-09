61aa6ecb7b34d88bc46ed2b7a716e65b04e862f1
novi <novi_dewi@kamoro.com>
2024-09-09 20:35:55 +0700

    restructure with hapi plugin

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,29 @@
-const { addNoteHandler, getAllNotesHandler, getNotesByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handlers');
-
-const routes = [
+const routes = (handler) => [
   {
     method: 'POST',
     path: '/notes',
-    handler: addNoteHandler,
+    handler: handler.postNoteHandler,
   },
   {
     method: 'GET',
     path: '/notes',
-    handler: getAllNotesHandler
+    handler: handler.getNotesHandler,
   },
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: getNotesByIdHandler
+    handler: handler.getNoteByIdHandler,
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
-    handler: editNoteByIdHandler
+    handler: handler.putNoteByIdHandler,
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
-    handler: deleteNoteByIdHandler
-  }
+    handler: handler.deleteNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
